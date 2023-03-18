6aebdcf3615e5c99a8778591d1b9dcca7194c5c5
Ernest <ernestwinata@gmail.com>
2023-03-18 08:03:11 -0700

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun(chore = "roller-skate"){
+    return `This Saturday, I want to ${chore}!`;
+
+console.log(saturdayFun())
+console.log(saturdayFun("bathe my dog"));
+}
+
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+
+console.log(mondayWork())
+console.log(mondayWork("work from home"));
+}
+
+function wrapAdjective(placeholder = "*") {
+    return function(compliment = "special") {
+      return `You are ${placeholder}${compliment}${placeholder}!`;
+    };
+  }
+  const praise = wrapAdjective("!");
+  console.log(praise("a hard worker")); 
+  console.log(praise("a dedicated programmer")); 
+
+  
